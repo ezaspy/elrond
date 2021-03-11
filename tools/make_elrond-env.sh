5b8ec8f21d69504f12c629bc6061a8b4f412ab72
ezaspy <ezaspython@gmail.com>
2021-03-11 21:58:32 +0000

    Additional profiles
    Additional volatility2.6 profiles added
    volatility2.6 complete
    Modifications made to elrond-env script

--- tools/make_elrond-env.sh
+++ tools/make_elrond-env.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 mkdir ~/Desktop/posters
 mv ~/Desktop/*.pdf ~/Desktop/posters/
+sudo chmod 744 /opt/elrond/tools/elrond_background.jpg
 gsettings set org.gnome.desktop.background picture-uri "file:///opt/elrond/tools/elrond_background.jpg"
 cd ..
 export PATH=$PATH:/opt/elrond
 sudo chmod +x /opt/elrond/elrond.py
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.7
+sudo apt-get install python3.7 -y
 sudo git clone https://github.com/volatilityfoundation/volatility3
 sudo mv volatility3/ /usr/lib/python3.7/
 sudo chmod -R 755 /usr/lib/python3.7/volatility3/
 sudo chown -R root:root /usr/lib/python3.7/volatility3/
-sudo apt install libimage-exiftool-perl
-sudo apt install libbz2-dev libattr1-dev cmake cmake-curses-gui
+sudo apt install libimage-exiftool-perl -y
+sudo apt install libbz2-dev libattr1-dev cmake cmake-curses-gui -y
 sudo apt-get install john -y
 cd /usr/local/bin
 sudo git clone https://github.com/ezaspy/apfs-fuse.git
@@ -24,13 +25,12 @@ sudo mkdir build
 cd build
 sudo cmake ..
 sudo ccmake .
-echo " Now just run the following commands:"
-echo "   cd /usr/local/bin/apfs-fuse/build/"
-echo "   sudo make"
-echo "   cd ~/elrond"
-sleep 5
-echo " Press ENTER to continue..."
+echo "Almost finished... now just run the following commands:"
+echo " cd /usr/local/bin/apfs-fuse/build/ && sudo make"
+echo " cd /opt/elrond"
+sleep 4
+echo "Press ENTER to continue..."
 read answer
-echo " Enjoy elrond. Bye!"
-sleep 2
+echo " Enjoy elrond - bye!"
+sleep 1
 kill $PPID
