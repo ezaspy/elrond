3df55678932ee92d72f4597c1714d556c274e3bc
ezaspy <42219366+ezaspy@users.noreply.github.com>
2021-06-18 14:31:49 +0100

    Update log.py

--- rivendell/audit/log.py
+++ rivendell/audit/log.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3 -tt
 
 def doLog(verbosity, outdir, entry, prnt):
-    if "Datetime,Host,Stage,Information" in entry:
+    if "elrond_time,elrond_host,elrond_stage,elrond_log_entry" in entry:
         writemode = "w"
     else:
         writemode = "a"
-    with open(outdir+str(str(prnt.split("'")[-2]).split("/")[-1]).split("::")[0]+"/"+str(str(prnt.split("'")[-2]).split("/")[-1]).split("::")[0]+".log", writemode) as logentry:
-        logentry.write(entry)
+    with open(outdir+str(str(prnt.split("'")[-2]).split("/")[-1]).split("::")[0]+"/audit.log", writemode) as logentry:
+        logentry.write(entry.replace("'",""))
     if verbosity == "veryverbose_verbose" or verbosity == "veryverbose":
         print(prnt)
     else:
