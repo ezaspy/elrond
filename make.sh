fdc6d4c023f2d56a15f44d05d80cf6b18358dde9
ezaspy <42219366+ezaspy@users.noreply.github.com>
2021-12-15 15:11:12 +0000

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 sudo mv ../elrond/elrond /opt/elrond
-sudo mv ../LICENSE /opt/elrond
-sudo mv ../README.md /opt/elrond
+sleep 1
+sudo mv ./LICENSE /opt/elrond
+sudo mv ./README.md /opt/elrond
+sleep 1
 sudo chmod -R 744 /opt/elrond/
 export PATH=$PATH:/opt/elrond
 sudo chmod +x /opt/elrond/config.sh
 sudo chmod +x /opt/elrond/elrond.py
 clear
 cd /opt/elrond
+sleep 4
 sudo ./config.sh
