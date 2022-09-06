150750253519e75e57b3082e7eb53ee8f8dc34fc
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-09-06 20:56:11 +0100

    config tools

--- elrond/tools/config/splunk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-printf "\n [+] Downloading Splunk...\n\n"
-wget -O /opt/elrond/elrond/tools/.splunk-9.0.0.1-9e907cedecb1-linux-2.6-amd64.deb "https://download.splunk.com/products/splunk/releases/9.0.0.1/linux/splunk-9.0.0.1-9e907cedecb1-linux-2.6-amd64.deb"
-sudo updatedb
-clear
-sleep 2
