1c1bc82653fd64a727f2c1c3eabf8478b4e64fcd
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-09-07 19:44:54 +0100

    Update config.sh

--- elrond/config.sh
+++ elrond/config.sh
@@ -10,19 +10,6 @@ sudo apt update
 ../elrond/tools/config/scripts/./splunk.sh
 #../elrond/tools/config/scripts/./elastic.sh
 ../elrond/tools/config/scripts/./navigator.sh
-sudo updatedb
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.9 python3-pip mlocate build-essential yara john gparted -y --fix-missing
-PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
-git clone https://github.com/harelsegev/INDXRipper /opt/elrond/elrond/tools/INDXRipper
-/usr/bin/python3.9 -m pip install --upgrade pip
-/usr/bin/python3 -m pip install --upgrade pip
-/usr/bin/python -m pip install --upgrade pip
-sudo python3.9 -m pip install setuptools
-sudo python3.9 -m pip install construct==2.10.68
-python3.9 -m pip install setuptools
-python3.9 -m pip install construct==2.10.68
 updatedb
 # configuring elrond
 sudo chmod -R 744 /opt/elrond/
