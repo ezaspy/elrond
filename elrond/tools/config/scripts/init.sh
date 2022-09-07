d8169f33423f9bae1e38651eb11f9aba52baaaf6
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-09-07 19:44:52 +0100

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -20,8 +20,16 @@ echo "/dev/sdb swap swap defaults 0 0" >> /etc/fstab
 sudo chmod 664 /etc/fstab
 sleep 1
 printf "\n  -> Running initialization script for elrond...\n\n"
-sudo apt install mlocate build-essential yara john gparted -y --fix-missing
-updatedb
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.9 python3-pip mlocate build-essential yara john gparted -y --fix-missing
+PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+git clone https://github.com/harelsegev/INDXRipper /opt/elrond/elrond/tools/INDXRipper
+/usr/bin/python3.9 -m pip install --upgrade pip && /usr/bin/python3 -m pip install --upgrade pip && /usr/bin/python -m pip install --upgrade pip
+sudo python3.9 -m pip install setuptools && sudo python3.9 -m pip install construct==2.10.68
+python3.9 -m pip install setuptools && python3.9 -m pip install construct==2.10.68
+echo "[Wallet]\nEnabled=false" >> /home/sansforensics/.config/kwalletrc
+sudo updatedb
 # configuring elrond
 sudo chmod -R 744 /opt/elrond/
 export PATH=$PATH:/opt/elrond
