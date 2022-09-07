e4db6a2b52782aa8557a7f3bafb16b304508483d
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-09-07 20:25:33 +0100

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sleep 1
 clear
+printf "\n  -> Running initialization script for elrond...\n\n"
 # installing vmware_tools
 sudo tar -xvf /opt/elrond/elrond/tools/config/VMwareTools-10.3.23-16594550.tar.gz
 cd vmware-tools-distrib
@@ -18,17 +19,8 @@ sudo cp /etc/fstab /etc/fstab.orig
 sudo chmod 777 /etc/fstab
 echo "/dev/sdb swap swap defaults 0 0" >> /etc/fstab
 sudo chmod 664 /etc/fstab
-sleep 1
-printf "\n  -> Running initialization script for elrond...\n\n"
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
+../elrond/tools/config/scripts/./splunk.sh
 sudo apt install python3.9 python3-pip mlocate build-essential yara john gparted -y --fix-missing
-PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
-git clone https://github.com/harelsegev/INDXRipper /opt/elrond/elrond/tools/INDXRipper
-/usr/bin/python3.9 -m pip install --upgrade pip && /usr/bin/python3 -m pip install --upgrade pip && /usr/bin/python -m pip install --upgrade pip
-sudo python3.9 -m pip install setuptools && sudo python3.9 -m pip install construct==2.10.68
-python3.9 -m pip install setuptools && python3.9 -m pip install construct==2.10.68
-echo "[Wallet]\nEnabled=false" >> /home/sansforensics/.config/kwalletrc
 sudo updatedb
 # configuring elrond
 sudo chmod -R 744 /opt/elrond/
