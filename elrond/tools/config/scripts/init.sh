1543122e35d1cc82955a13f295a02597debf2212
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-09-07 22:22:49 +0100

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -4,11 +4,13 @@ clear
 printf "\n  -> Running initialization script for elrond...\n\n"
 # installing vmware_tools
 sudo tar -xvf /opt/elrond/elrond/tools/config/VMwareTools-10.3.23-16594550.tar.gz
+sleep 2
 cd vmware-tools-distrib
 sudo rm -rf vmware-install.pl
-sudo mv /opt/elrond/elrond/tools/config/vmware-install.pl .
+sudo cp /opt/elrond/elrond/tools/config/vmware-install.pl .
 sudo chmod 755 vmware-install.pl
 sudo ./vmware-install.pl -f
+yes '' | sudo ./vmware-install.pl
 cd ..
 sudo rm -rf vmware-tools-distrib
 sleep 1
@@ -19,7 +21,7 @@ sudo cp /etc/fstab /etc/fstab.orig
 sudo chmod 777 /etc/fstab
 echo "/dev/sdb swap swap defaults 0 0" >> /etc/fstab
 sudo chmod 664 /etc/fstab
-../elrond/tools/config/scripts/./splunk.sh
+../elrond/tools/config/scripts/./python.sh
 sudo apt install mlocate build-essential yara john gparted -y --fix-missing
 sudo updatedb
 # configuring elrond
