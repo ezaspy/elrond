976af1f1ddc6f0dfa931113cb404bead1b21bd17
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-05 20:08:07 +0000

    Update python.sh

--- elrond/tools/config/scripts/python.sh
+++ elrond/tools/config/scripts/python.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 sleep 1
 clear
-printf "\n  -> Configuring python3.9...\n\n"
+printf "\n  -> Configuring python3.8...\n\n"
 yes '' | sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
 PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 git clone https://github.com/harelsegev/INDXRipper /opt/elrond/elrond/tools/INDXRipper
-/usr/bin/python3.9 -m pip install --upgrade pip
+/usr/bin/python3.8 -m pip install --upgrade pip
 /usr/bin/python3 -m pip install --upgrade pip
 /usr/bin/python -m pip install --upgrade pip
-sudo python3.9 -m pip install setuptools
-sudo python3.9 -m pip install construct==2.10.68
-python3.9 -m pip install setuptools
-python3.9 -m pip install construct==2.10.68
+sudo python3.8 -m pip install setuptools
+sudo python3.8 -m pip install construct==2.10.68
+python3.8 -m pip install setuptools
+python3.8 -m pip install construct==2.10.68
 python3 -m pip install construct
 python3 -m pip install etl
 python3 -m pip install etl-parser
