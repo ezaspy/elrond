90af10d8a64f3e8966ad2bbb7e21c7c5eb9f2180
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 12:33:10 +0000

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -5,15 +5,8 @@ printf "\n  -> Running initialization script for elrond...\n\n"
 echo "[Wallet]\nEnabled=false" > /home/sansforensics/.config/kwalletrc
 # installing vmware_tools
 sudo tar -xvf /opt/elrond/elrond/tools/config/VMwareTools-10.3.23-16594550.tar.gz
-sleep 2
-cd vmware-tools-distrib
-sudo rm -rf vmware-install.pl
-sudo cp /opt/elrond/elrond/tools/config/vmware-install.pl .
-sudo chmod 755 vmware-install.pl
-yes '' | sudo ./vmware-install.pl -f
-cd ..
-sudo rm -rf vmware-tools-distrib
-sleep 1
+sleep 1 && cd vmware-tools-distrib && sudo rm -rf vmware-install.pl && sudo cp /opt/elrond/elrond/tools/config/vmware-install.pl . && sudo chmod 755 vmware-install.pl && yes '' | sudo ./vmware-install.pl -f
+cd .. && sudo rm -rf vmware-tools-distrib && sleep 1
 # creating linux_swap space
 sudo mkswap /dev/sdb
 sudo swapon /dev/sdb
@@ -24,15 +17,10 @@ sudo chmod 664 /etc/fstab
 /opt/elrond/elrond/tools/config/scripts/./python.sh
 sudo wget -O /opt/elrond/elrond/tools/.clamav-0.105.1-2.linux.x86_64.deb "https://www.clamav.net/downloads/production/clamav-0.105.1-2.linux.x86_64.deb"
 sudo dpkg -i /opt/elrond/elrond/tools/.clamav-0.105.1-2.linux.x86_64.deb
-sudo apt update
-sudo apt install mlocate build-essential qemu wget apt-transport-https software-properties-common mdbtools yara clamav clamav-daemon john gparted -y --fix-missing
-sudo systemctl stop clamav-freshclam
-sudo freshclam
-sudo systemctl start clamav-freshclam
-sudo updatedb
+sudo apt update && sudo apt install mlocate build-essential qemu wget apt-transport-https software-properties-common mdbtools yara clamav clamav-daemon john gparted -y --fix-missing
+sudo systemctl stop clamav-freshclam && sudo freshclam && sudo systemctl start clamav-freshclam
 # configuring elrond
-sudo chmod -R 744 /opt/elrond/
-export PATH=$PATH:/opt/elrond
-sudo chmod +x /opt/elrond/elrond.py
+sudo chmod -R 744 /opt/elrond/ && sudo chmod +x /opt/elrond/elrond.py
+echo "export PATH=$PATH:/opt/elrond" > /home/sansforensics/.bashrc
 sudo updatedb
 sleep 1
