8def7fff888f19508e4e087181e17a3185c0aab5
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 15:32:52 +0000

    Update config.sh

--- elrond/config.sh
+++ elrond/config.sh
@@ -10,7 +10,7 @@ cd /opt/elrond/elrond
 /opt/elrond/elrond/tools/config/scripts/./splunk.sh
 /opt/elrond/elrond/tools/config/scripts/./elastic.sh
 /opt/elrond/elrond/tools/config/scripts/./navigator.sh
-/opt/elrond/elrond/tools/config/scripts/./nrsl.sh
+/opt/elrond/elrond/tools/config/scripts/./nsrl.sh
 sudo chmod -R 744 /opt/elrond/ && sudo chmod +x /opt/elrond/elrond.py
 echo "export PATH=$PATH:/opt/elrond" > /home/sansforensics/.bashrc
 sudo updatedb
