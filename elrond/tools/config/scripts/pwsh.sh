aec798b6c95d4a25d9bbbc514eebb6966a793111
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 07:29:56 +0000

    Update pwsh.sh

--- elrond/tools/config/scripts/pwsh.sh
+++ elrond/tools/config/scripts/pwsh.sh
@@ -2,11 +2,10 @@
 sleep 1
 clear
 printf "\n  -> Downloading & Installing PowerShell...\n\n"
-wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+wget -q "https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb"
 sudo dpkg -i packages-microsoft-prod.deb
+sudo apt update
 sudo apt-get install powershell -y --fix-missing
 sudo rm -rf packages-microsoft-prod.deb
 sudo updatedb
 sleep 1
-
-
