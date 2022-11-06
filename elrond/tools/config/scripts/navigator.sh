e89dd317cefe48f01a4d0891e84daabf2b5d3468
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 15:47:51 +0000

    Update navigator.sh

--- elrond/tools/config/scripts/navigator.sh
+++ elrond/tools/config/scripts/navigator.sh
@@ -5,11 +5,10 @@ printf "\n  -> Installing & configuring attack-navigator...\n\n"
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt install nodejs yarn gcc g++ make -y --fix-missing
-sudo npm install -g npm@8.6.0
-sudo npm install -g @angular/cli
-sudo npm install -g pm2
+sudo apt-get update && sudo apt install nodejs yarn gcc g++ make -y --fix-missing
+sudo npm install -g npm@8.6.0 && sudo npm install -g @angular/cli && sudo npm install -g pm2
+#npm audit fix --force
+sleep 10000
 cd /opt/elrond/elrond/tools/ && git clone https://github.com/mitre-attack/attack-navigator.git
 sudo chmod -R 744 /opt/elrond/ && cd attack-navigator/nav-app
 npm install && npm audit fix --force && ng build && ng build
