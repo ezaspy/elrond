29471862137a8d6b9caee4524df29809752114ab
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 06:34:27 +0000

    Update python.sh

--- elrond/tools/config/scripts/python.sh
+++ elrond/tools/config/scripts/python.sh
@@ -4,8 +4,8 @@ clear
 printf "\n  -> Configuring python3.9...\n\n"
 yes '' | sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.9
-#PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+sudo apt install python3.9 -y --fix-missing
+PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 git clone https://github.com/harelsegev/INDXRipper /opt/elrond/elrond/tools/INDXRipper
 /usr/bin/python3.9 -m pip install --upgrade pip
 /usr/bin/python3 -m pip install --upgrade pip
