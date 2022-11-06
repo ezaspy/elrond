b1cb923438fcaa9e8870c2fb6205085215f67d5d
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 12:33:06 +0000

    Update python.sh

--- elrond/tools/config/scripts/python.sh
+++ elrond/tools/config/scripts/python.sh
@@ -8,12 +8,13 @@ sudo apt install python3.9 -y --fix-missing
 PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 sudo git clone https://github.com/harelsegev/INDXRipper /opt/elrond/elrond/tools/INDXRipper
 /usr/bin/python3.9 -m pip install --upgrade pip
+/usr/bin/python3.8 -m pip install --upgrade pip
 /usr/bin/python3 -m pip install --upgrade pip
 /usr/bin/python -m pip install --upgrade pip
 python3.9 -m pip install setuptools
 python3.9 -m pip install construct==2.10.68
-python3.9 -m pip install setuptools
-python3.9 -m pip install construct==2.10.68
+python3.8 -m pip install setuptools
+python3.8 -m pip install construct==2.10.68
 python3 -m pip install construct
 python3 -m pip install etl
 python3 -m pip install etl-parser
