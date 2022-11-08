4dd2f73a4f5ed6c1e14c3686e588f2e0d83afae9
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 15:11:59 +0000

    Update elastic.sh

--- elrond/tools/config/scripts/elastic.sh
+++ elrond/tools/config/scripts/elastic.sh
@@ -7,16 +7,10 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo apt install apt-transport-https -y
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
-sudo apt install elasticsearch
+sudo apt install elasticsearch logstash kibana
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
+sudo /bin/systemctl enable logstash.service
+sudo /bin/systemctl enable kibana.service
 sudo sysctl -w vm.max_map_count=262144
-sudo apt update
-sudo apt install logstash
-sudo systemctl enable logstash.service
-sudo apt update
-sudo apt install kibana
-sudo /bin/systemctl daemon-reload
-sudo systemctl enable kibana.service
 sudo updatedb
-sleep 1000
