3f918a4909ae1955c3afc1deb3e046fe708622b7
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 18:41:44 +0000

    Update navigator.sh

--- elrond/tools/config/scripts/navigator.sh
+++ elrond/tools/config/scripts/navigator.sh
@@ -2,20 +2,19 @@
 sleep 1
 clear
 printf "\n  -> Installing & configuring attack-navigator...\n\n"
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - && sleep 1
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null && sleep 1
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sleep 1
 sudo apt-get update
-sudo apt install nodejs yarn gcc g++ make -y --fix-missing
-sudo npm install -g npm@8.6.0
-sudo npm install -g @angular/cli
-sudo npm install -g pm2
-sudo git clone https://github.com/mitre-attack/attack-navigator.git /opt/elrond/elrond/tools/attack-navigator
-sudo chmod -R 755 /opt/elrond/
-cd /opt/elrond/elrond/tools/attack-navigator/nav-app
-sudo npm install
-sudo npm audit fix
-sudo ng build
+sudo apt install nodejs yarn gcc g++ make -y --fix-missing && sleep 4
+sudo npm install -g npm@8.6.0 && sleep 4
+sudo npm install -g @angular/cli && sleep 4
+sudo npm install -g pm2 && sleep 4
+sudo git clone https://github.com/mitre-attack/attack-navigator.git /opt/elrond/elrond/tools/attack-navigator && sleep 1
+sudo chmod -R 755 /opt/elrond/ && sleep 1
+cd /opt/elrond/elrond/tools/attack-navigator/nav-app && sleep 4
+sudo npm install && sleep 4
+sudo ng build && sleep 4
 sudo cp -r dist/ /var/www/html/
 sudo mv /var/www/html/dist/ /var/www/html/attack-navigator/
 sudo service apache2 restart
