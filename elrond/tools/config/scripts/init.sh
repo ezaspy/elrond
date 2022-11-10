1cab311bfe70338a91eef6c6d5c5fae9a5101660
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-10 19:13:28 +0000

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -2,7 +2,6 @@
 sleep 1
 clear
 printf "\n  -> Running initialization script for elrond...\n\n"
-echo "[Wallet]\nEnabled=false" > /home/sansforensics/.config/kwalletrc
 # installing vmware_tools
 sudo tar -xvf /opt/elrond/elrond/tools/config/VMwareTools-10.3.23-16594550.tar.gz
 sleep 1 && cd vmware-tools-distrib && sudo rm -rf vmware-install.pl && sudo cp /opt/elrond/elrond/tools/config/vmware-install.pl . && sudo chmod 755 vmware-install.pl && yes '' | sudo ./vmware-install.pl -f
@@ -14,6 +13,8 @@ sudo cp /etc/fstab /etc/fstab.orig
 sudo chmod 777 /etc/fstab
 echo "/dev/sdb swap swap defaults 0 0" >> /etc/fstab
 sudo chmod 664 /etc/fstab
+# downloading software
+python3 -m keyring --disable
 /opt/elrond/elrond/tools/config/scripts/./python.sh
 sudo wget -O /opt/elrond/elrond/tools/.clamav-0.105.1-2.linux.x86_64.deb "https://www.clamav.net/downloads/production/clamav-0.105.1-2.linux.x86_64.deb"
 sudo dpkg -i /opt/elrond/elrond/tools/.clamav-0.105.1-2.linux.x86_64.deb
