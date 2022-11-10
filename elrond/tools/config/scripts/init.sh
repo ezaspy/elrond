209000499e72620138acccd7a2083831053a3e29
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-10 14:02:47 +0000

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -20,7 +20,7 @@ sudo dpkg -i /opt/elrond/elrond/tools/.clamav-0.105.1-2.linux.x86_64.deb
 sudo apt update && sudo apt install mlocate build-essential qemu wget apt-transport-https software-properties-common mdbtools yara clamav clamav-daemon john gparted -y --fix-missing
 sudo systemctl stop clamav-freshclam && sudo freshclam && sudo systemctl start clamav-freshclam
 # configuring elrond
-sudo chmod -R 744 /opt/elrond/ && sudo chmod +x /opt/elrond/elrond.py
+sudo chmod -R 744 /opt/elrond/ && sudo chmod +x /opt/elrond/elrond/elrond.py
 echo "export PATH=$PATH:/opt/elrond" > /home/sansforensics/.bashrc
 sudo updatedb
 sleep 1
