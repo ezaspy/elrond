5bfae153c57f56c1be13e578734f9d7a4d11c7ca
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-14 15:47:17 +0000

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -3,9 +3,7 @@ sleep 1
 clear
 printf "\n  -> Running initialization script for elrond...\n\n"
 # installing vmware_tools
-sudo tar -xvf /opt/elrond/elrond/tools/config/VMwareTools-10.3.23-16594550.tar.gz
-sleep 1 && cd vmware-tools-distrib && sudo rm -rf vmware-install.pl && sudo cp /opt/elrond/elrond/tools/config/vmware-install.pl . && sudo chmod 755 vmware-install.pl && yes '' | sudo ./vmware-install.pl -f
-cd .. && sudo rm -rf vmware-tools-distrib && sleep 1
+/opt/elrond/elrond/tools/config/scripts/./virtual.sh
 # creating linux_swap space
 sudo mkswap /dev/sdb
 sudo swapon /dev/sdb
