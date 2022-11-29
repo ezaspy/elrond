9802cc86e42591ee654a873e4c1d0be2ee3cf29d
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-29 18:51:06 +0000

    Update config.sh

--- elrond/config.sh
+++ elrond/config.sh
@@ -4,17 +4,19 @@ clear
 cd /opt/elrond/elrond
 /opt/elrond/elrond/tools/config/scripts/./init.sh
 /opt/elrond/elrond/tools/config/scripts/./apfs-fuse.sh
-/opt/elrond/elrond/tools/config/scripts/./pwsh.sh
 /opt/elrond/elrond/tools/config/scripts/./volatility3.sh
 /opt/elrond/elrond/tools/config/scripts/./dwarf2json.sh
 /opt/elrond/elrond/tools/config/scripts/./splunk.sh
 /opt/elrond/elrond/tools/config/scripts/./elastic.sh
 /opt/elrond/elrond/tools/config/scripts/./navigator.sh
 /opt/elrond/elrond/tools/config/scripts/./nsrl.sh
-sudo wget -O /opt/elrond/elrond/tools/.clamav.deb "https://www.clamav.net/downloads/production/clamav-0.105.1-2.linux.x86_64.deb"
-sudo dpkg -i /opt/elrond/elrond/tools/.clamav.deb
+wget -q "https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb"
+sudo dpkg -i packages-microsoft-prod.deb
+wget -q "https://www.clamav.net/downloads/production/clamav-0.105.1-2.linux.x86_64.deb"
+sudo dpkg -i clamav-0.105.1-2.linux.x86_64.deb
 sudo apt update
-sudo apt install mlocate net-tools build-essential qemu apt-transport-https software-properties-common systemd gnupg sqlite3 mdbtools yara clamav clamav-daemon john gparted -y --fix-missing
+sudo apt install mlocate net-tools build-essential qemu apt-transport-https software-properties-common systemd gnupg powershell sqlite3 mdbtools yara clamav clamav-daemon john gparted -y --fix-missing
+sudo rm -rf packages-microsoft-prod.deb
 sudo systemctl stop clamav-freshclam
 sudo freshclam
 sudo systemctl start clamav-freshclam
