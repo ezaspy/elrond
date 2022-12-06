b23e89d49f131087dcfb4244368628205ce89081
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-06 09:59:34 +0000

    Update config.sh

--- elrond/config.sh
+++ elrond/config.sh
@@ -11,18 +11,6 @@ cd /opt/elrond/elrond
 /opt/elrond/elrond/tools/config/scripts/./elastic.sh
 /opt/elrond/elrond/tools/config/scripts/./navigator.sh
 /opt/elrond/elrond/tools/config/scripts/./nsrl.sh
-wget -q "https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb"
-sudo dpkg -i packages-microsoft-prod.deb
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - # vscode
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" # vscode
-wget -q "https://www.clamav.net/downloads/production/clamav-0.105.1-2.linux.x86_64.deb"
-sudo dpkg -i clamav-0.105.1-2.linux.x86_64.deb
-sudo apt update
-sudo apt install mlocate net-tools build-essential qemu apt-transport-https software-properties-common systemd gnupg powershell code sqlite3 mdbtools yara clamav clamav-daemon john gparted -y --fix-missing
-sudo rm -rf packages-microsoft-prod.deb
-sudo systemctl stop clamav-freshclam
-sudo freshclam
-sudo systemctl start clamav-freshclam
 sudo chmod -R 755 /opt/elrond/
 sudo chmod +x /opt/elrond/elrond/elrond.py
 echo "export PATH=$PATH:/opt/elrond" >> /home/sansforensics/.bashrc
