fcc50e665671a9d53714846ffe70a54e79f61388
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-06 09:59:32 +0000

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -11,6 +11,19 @@ sudo cp /etc/fstab /etc/fstab.orig
 sudo chmod 777 /etc/fstab
 echo "/dev/sdb swap swap defaults 0 0" >> /etc/fstab
 sudo chmod 664 /etc/fstab
+# installing required software
+wget -q "https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb"
+sudo dpkg -i packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - # vscode
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" # vscode
+wget -q "https://www.clamav.net/downloads/production/clamav-0.105.1-2.linux.x86_64.deb"
+sudo dpkg -i clamav-0.105.1-2.linux.x86_64.deb
+sudo apt update
+sudo apt install mlocate net-tools build-essential qemu apt-transport-https software-properties-common systemd gnupg xz-utils powershell code sqlite3 mdbtools yara clamav clamav-daemon john gparted -y --fix-missing
+sudo rm -rf packages-microsoft-prod.deb
+sudo systemctl stop clamav-freshclam
+sudo freshclam
+sudo systemctl start clamav-freshclam
 # downloading indx-parser
 python3 -m keyring --disable
 /opt/elrond/elrond/tools/config/scripts/./indx.sh
