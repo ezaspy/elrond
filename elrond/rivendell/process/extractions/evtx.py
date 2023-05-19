6ccdf7f1bbd84ca256f05c11709ccfeba4f2e787
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-05-19 16:53:57 +0100

    Update evtx.py

--- elrond/rivendell/process/extractions/evtx.py
+++ elrond/rivendell/process/extractions/evtx.py
@@ -127,6 +127,7 @@ def extract_evtx(
                 .replace('"}"\', \'"{"', '"}, {"')
                 .replace('[\'"{"', '[{"')
                 .replace('"}"\']', '"}]')
+                .replace("/\"", "/")
                 .replace("ProcessName", "WinProcess")
             )
         else:
