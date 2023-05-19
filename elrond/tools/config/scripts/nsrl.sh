74a19ab497438842a56e3c5181730ea0d7bfd2ec
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-05-19 16:53:21 +0100

    Update nsrl.sh

--- elrond/tools/config/scripts/nsrl.sh
+++ elrond/tools/config/scripts/nsrl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 1
 clear
-wget https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/current/rds_modernm.zip > /opt/elrond/elrond/tools/.rds_modernm.zip
-unzip /opt/elrond/elrond/tools/.rds_modernm.zip -d /opt/elrond/elrond/tools/
+printf "\n\n  -> OPTIONAL: Download 'https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/current_rdsv3/RDS_2022.12.1_modern_delta.zip' via your chosen browser and save the file to '/opt/elrond/elrond/tools/'. Press ENTER to continue..."
+read answer
 sleep 1
