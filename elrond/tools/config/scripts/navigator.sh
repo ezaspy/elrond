b15caef0ce1ca72dcd4cd045f823297d8a59ff44
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-05-19 16:53:24 +0100

    Update navigator.sh

--- elrond/tools/config/scripts/navigator.sh
+++ elrond/tools/config/scripts/navigator.sh
@@ -7,21 +7,23 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /us
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sleep 1
 sudo apt-get update
 sudo apt install nodejs yarn gcc g++ make -y --fix-missing
-sleep 2
+sleep 1
 sudo npm install -g npm@8.6.0
-sleep 2
+sleep 1
 sudo npm install -g @angular/cli
-sleep 2
+sleep 1
 sudo npm install -g pm2
-sleep 2
+sleep 1
 sudo git clone https://github.com/mitre-attack/attack-navigator.git /opt/elrond/elrond/tools/attack-navigator && sleep 1
 sudo chmod -R 755 /opt/elrond/ && sleep 1
 cd /opt/elrond/elrond/tools/attack-navigator/nav-app
-sleep 2
+sleep 1
 sudo npm install
-sleep 2
+sleep 1
 sudo ng build
-sleep 2
+sleep 1
+npm install -g npm@9.6.7
+sleep 1
 sudo cp -r dist/ /var/www/html/
 sudo mv /var/www/html/dist/ /var/www/html/attack-navigator/
 sudo service apache2 restart
