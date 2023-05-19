c6be72f54f5b366ab6f0e02dbb355b0686500a85
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-05-19 16:54:10 +0100

    removed logstash

--- elrond/tools/config/scripts/elastic.sh
+++ elrond/tools/config/scripts/elastic.sh
@@ -7,9 +7,11 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo apt install apt-transport-https -y
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
-sudo apt install elasticsearch logstash kibana
+sudo apt install elasticsearch kibana
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
-sudo /bin/systemctl enable logstash.service
 sudo /bin/systemctl enable kibana.service
 sudo sysctl -w vm.max_map_count=262144
+echo vm.max_map_count=262144 | sudo tee -a /etc/sysctl.conf
+sleep 1
+sudo sysctl -p
