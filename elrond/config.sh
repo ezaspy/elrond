0637ea300c82ccbd8465d1eb9026d0b56e2c0d58
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-05-21 16:16:12 +0100

    Update config.sh

--- elrond/config.sh
+++ elrond/config.sh
@@ -5,12 +5,13 @@ sudo apt update
 cd /opt/elrond/elrond
 /opt/elrond/elrond/tools/config/scripts/./init.sh
 /opt/elrond/elrond/tools/config/scripts/./apfs-fuse.sh
+/opt/elrond/elrond/tools/config/scripts/./nsrl.sh
 /opt/elrond/elrond/tools/config/scripts/./volatility3.sh
 /opt/elrond/elrond/tools/config/scripts/./dwarf2json.sh
 /opt/elrond/elrond/tools/config/scripts/./splunk.sh
 /opt/elrond/elrond/tools/config/scripts/./elastic.sh
 /opt/elrond/elrond/tools/config/scripts/./navigator.sh
-/opt/elrond/elrond/tools/config/scripts/./nsrl.sh
+clear
 sudo chmod -R 755 /opt/elrond/
 sudo chmod +x /opt/elrond/elrond/elrond.py
 echo "export PATH=$PATH:/opt/elrond" >> /home/sansforensics/.bashrc
