3453210b289471745037c586600bd927c00b5d1b
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-05-21 16:14:12 +0100

    Update nsrl.sh

--- elrond/tools/config/scripts/nsrl.sh
+++ elrond/tools/config/scripts/nsrl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 1
 clear
-printf "\n\n  -> OPTIONAL: Download 'https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/current_rdsv3/RDS_2022.12.1_modern_delta.zip' via your chosen browser and save the file to '/opt/elrond/elrond/tools/'. Press ENTER to continue..."
+printf "\n\n  -> OPTIONAL:\n      Download 'https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/current_rdsv3/RDS_2022.12.1_modern_delta.zip'\n      via your chosen browser and save the file to '/opt/elrond/elrond/tools/'\n\n     Press ENTER to continue..."
 read answer
 sleep 1
