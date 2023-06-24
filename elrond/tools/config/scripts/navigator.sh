30c76046248c4a5b9a81fe8cb1ff0ac75c25bb2f
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-06-24 08:46:23 +0100

    Update navigator.sh

--- elrond/tools/config/scripts/navigator.sh
+++ elrond/tools/config/scripts/navigator.sh
@@ -2,11 +2,14 @@
 sleep 1
 clear
 printf "\n  -> Installing & configuring attack-navigator...\n\n"
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - && sleep 1
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null && sleep 1
-echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sleep 1
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sleep 1
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+sleep 1
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sleep 1
 sudo apt-get update
-sudo apt install nodejs yarn gcc g++ make -y --fix-missing
+sudo apt install npm nodejs yarn gcc g++ make -y --fix-missing
 sleep 1
 sudo npm install -g npm@8.6.0
 sleep 1
@@ -14,8 +17,10 @@ sudo npm install -g @angular/cli
 sleep 1
 sudo npm install -g pm2
 sleep 1
-sudo git clone https://github.com/mitre-attack/attack-navigator.git /opt/elrond/elrond/tools/attack-navigator && sleep 1
-sudo chmod -R 755 /opt/elrond/ && sleep 1
+sudo git clone https://github.com/mitre-attack/attack-navigator.git /opt/elrond/elrond/tools/attack-navigator
+sleep 1
+sudo chmod -R 755 /opt/elrond/
+sleep 1
 cd /opt/elrond/elrond/tools/attack-navigator/nav-app
 sleep 1
 sudo npm install
