b7870505d49efb66b31594da2df02520e3ff2d5a
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-06-26 09:10:46 +0100

    Update elastic.sh

--- elrond/tools/config/scripts/elastic.sh
+++ elrond/tools/config/scripts/elastic.sh
@@ -5,8 +5,7 @@ printf "\n  -> Installing & configuring elasticsearch...\n\n"
 sudo apt install curl jq openjdk-16-jre-headless -y
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt install apt-transport-https -y
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
-#echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt update
 sudo apt install elasticsearch kibana
 sudo /bin/systemctl daemon-reload
