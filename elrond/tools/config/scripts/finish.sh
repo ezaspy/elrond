7e8863af2750649e8408697c80221b36b8f9d9cd
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-07-23 14:44:07 +0100

    Update finish.sh

--- elrond/tools/config/scripts/finish.sh
+++ elrond/tools/config/scripts/finish.sh
@@ -4,16 +4,16 @@ echo fs.inotify.max_user_watches=1048576 | sudo tee -a /etc/sysctl.conf
 echo '
 export PS1="\e[1;36m\u@\h:\e[m \e[0;32m\w\e[m\n$ "' >> /home/sansforensics/.bashrc
 sudo chmod -R 755 /opt/elrond/
-chown -R sansforensics:sansforensics /opt/elrond
+sudo chown -R sansforensics:sansforensics /opt/elrond
 sudo chmod +x /opt/elrond/elrond/elrond.py
 echo "export PATH=$PATH:/opt/elrond" >> /home/sansforensics/.bashrc
-sudo updatedb
 cd /opt/elrond/elrond/
 clear
 printf "\n\n  -> SIFT has been successfully configured for elrond. Press ENTER to continue..."
 read answer
 sleep 1
-cat /dev/null > ~/.bash_history
+sudo updatedb
+echo '' | sudo tee ~/.bash_history
 history -c
 sleep 1
 clear
