cc28257b6da04db7134093d778b8806b318b0eaa
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-07-26 23:18:09 +0100

    Update nsrl.sh

--- elrond/tools/config/scripts/nsrl.sh
+++ elrond/tools/config/scripts/nsrl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 1
 clear
-printf "\n\n +--------------------------------------------- \e[1;31mOPTIONAL\e[m: RDS Hash Sets Download ---------------------------------------------+\n | Download the latest \e[1;33mFULL SQL DOWNLOADS\e[m RDS Hash Sets from the following link and save in \e[1;33m/opt/elrond/elrond/tools/\e[m         |\n | \e[0;36mhttps://www.nist.gov/itl/ssd/software-quality-group/national-software-reference-library-nsrl/nsrl-download/current-rds.zip \e[m|\n +----------------------------------------------------------------------------------------------------------------------------+\n   Press ENTER to continue...\n"
+printf "\n\n +------------------------------------------- \e[1;31mOPTIONAL\e[m: RDS Hash Sets Download -------------------------------------------+\n | Download the latest \e[1;33mFULL SQL DOWNLOADS\e[m RDS Hash Sets from the following link and save in \e[1;33m/opt/elrond/elrond/tools/\e[m     |\n | \e[0;36mhttps://www.nist.gov/itl/ssd/software-quality-group/national-software-reference-library-nsrl/nsrl-download/current-rds \e[m|\n +------------------------------------------------------------------------------------------------------------------------+\n   Press ENTER to continue...\n"
 read answer
 sleep 1
