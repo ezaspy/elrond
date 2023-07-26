fbc50cb915f01aecf9c2df8371007cb996f891ba
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-07-26 18:08:02 +0100

    Update nsrl.sh

--- elrond/tools/config/scripts/nsrl.sh
+++ elrond/tools/config/scripts/nsrl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 1
 clear
-printf "\n\n  -> OPTIONAL:\n      Download 'https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/current_rdsv3/RDS_2022.12.1_modern_delta.zip'\n      via your chosen browser and save the file to '/opt/elrond/elrond/tools/'\n\n     Press ENTER to continue..."
+printf "\n\n +--------------------------------------------- \e[1;31mOPTIONAL\e[m: RDS Hash Sets Download ---------------------------------------------+\n | Download the latest \e[1;33mFULL SQL DOWNLOADS\e[m RDS Hash Sets from the following link and save in \e[1;33m/opt/elrond/elrond/tools/\e[m         |\n | \e[0;36mhttps://www.nist.gov/itl/ssd/software-quality-group/national-software-reference-library-nsrl/nsrl-download/current-rds.zip \e[m|\n +----------------------------------------------------------------------------------------------------------------------------+\n   Press ENTER to continue...\n"
 read answer
 sleep 1
