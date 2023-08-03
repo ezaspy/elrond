744784e52eac91a37ae51fd506332a3220bf3e2c
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-08-03 20:26:19 +0100

    Update update.sh

--- elrond/tools/config/scripts/update.sh
+++ elrond/tools/config/scripts/update.sh
@@ -4,6 +4,11 @@ sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
 sudo chmod -R 755 /opt/elrond/
 sudo chown -R sansforensics:sansforensics /opt/elrond/
 cd ~
+printf "\n\n  -> elrond has been successfully updated. Press ENTER to continue..."
+read answer
+sleep 1
 sudo updatedb
 echo '' | sudo tee ~/.bash_history
 history -c
+sleep 1
+clear
