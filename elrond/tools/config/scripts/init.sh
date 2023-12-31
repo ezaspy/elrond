5c2c8eeee9a8773346a64896e035d0efd3ff087e
ezaspy <42219366+ezaspy@users.noreply.github.com>
2023-12-31 08:33:00 +0000

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -7,6 +7,8 @@
 sudo cp /usr/share/regripper/rip.pl /usr/share/regripper/rip.pl.old
 sudo sed -i 's/my \$VERSION/# Add: Define the variable plugindir\nmy \$plugindir = File::Spec->catfile\(\$scriptdir, "plugins"\);\n\nmy \$VERSION/' /usr/share/regripper/rip.pl
 # creating linux_swap space
+sudo swapoff /dev/sdb
+sudo umount /dev/sdb
 sudo mkswap /dev/sdb
 sudo swapon /dev/sdb
 sudo cp /etc/fstab /etc/fstab.orig
@@ -14,11 +16,9 @@ sudo chmod 777 /etc/fstab
 echo "/dev/sdb swap swap defaults 0 0" >> /etc/fstab
 sudo chmod 664 /etc/fstab
 # installing additional features for elrond
-wget -q "https://www.clamav.net/downloads/production/clamav-0.105.1-2.linux.x86_64.deb"
-sudo dpkg -i clamav-0.105.1-2.linux.x86_64.deb
 sudo apt update
 sudo apt install mlocate net-tools build-essential qemu apt-transport-https software-properties-common systemd gnupg xz-utils sqlite3 mdbtools yara clamav clamav-daemon john gparted dos2unix -y --fix-missing
-sudo rm -rf packages-microsoft-prod.deb
+sudo apt update
 sudo systemctl stop clamav-freshclam
 sudo freshclam
 sudo systemctl start clamav-freshclam
