18eb3d84681eb2d2e942694b4c36326c0d5e9998
Ben Smith <ezaspy@proton.me>
2024-05-03 14:05:13 +0100

    Update init.sh

--- elrond/tools/config/scripts/init.sh
+++ elrond/tools/config/scripts/init.sh
@@ -12,7 +12,25 @@ echo "/dev/sdb swap swap defaults 0 0" >> /etc/fstab
 sudo chmod 664 /etc/fstab
 # installing additional features for elrond
 sudo apt update
-sudo apt install vim mlocate net-tools build-essential qemu apt-transport-https software-properties-common systemd gnupg xz-utils sqlite3 mdbtools yara clamav clamav-daemon john gparted dos2unix -y --fix-missing
+sudo apt install vim mlocate net-tools build-essential qemu apt-transport-https software-properties-common systemd gnupg xz-utils sqlite3 mdbtools yara clamav clamav-daemon john gparted dos2unix sqlitebrowser python3-apt wireshark cutter bless sqlitebrowser flameshot securityonion-networkminer libguestfs -y --fix-missing
+sudo snap install cyberchef
+sudo dpkg -i /tmp/vscode.deb
+# installing maltego
+sudo apt install openjdk-17-jdk openjdk-17-jre
+wget -O /tmp/Maltego.v4.7.0.deb https://downloads.maltego.com/maltego-v4/linux/Maltego.v4.7.0.deb
+sudo dpkg -i /tmp/Maltego.v4.7.0.deb
+# installing python libraries
+python3 -m pip install --upgrade pip
+python3 -m pip install requests pandas openpyxl jupyterlab notebook voila
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install requests pandas openpyxl jupyterlab notebook voila
+sudo chmod -R 744 /opt/elrond/
+sudo chown -R $USER:$USER /opt/elrond
+cd /opt/elrond/elrond/tools/etl-parser
+python3 -m pip install -e .
+cd /opt/elrond/elrond
+# cloning additional repositories
+/opt/elrond/elrond/tools/config/scripts/./clone.sh
 sudo apt update
 sudo systemctl stop clamav-freshclam
 sudo freshclam
