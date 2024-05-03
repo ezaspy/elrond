e8963ac6f9940164631cf73d1dbf135786b13122
Ben Smith <ezaspy@proton.me>
2024-05-03 15:37:41 +0100

    Update repo.sh

--- elrond/tools/config/scripts/repo.sh
+++ elrond/tools/config/scripts/repo.sh
@@ -8,7 +8,6 @@ source ~/.bashrc
 clear
 printf "\n  -> Downloading additional elrond repository components...\n\n"
 sudo add-apt-repository -y ppa:linuxgndu/sqlitebrowser > /dev/null 2>&1 # db browser for sqlite
-sudo add-apt-repository -y ppa:securityonion/stable > /dev/null 2>&1 # network-miner
 # downloading additional tools for elrond
 gdown https://drive.google.com/uc?id=1mVrkLp84jchHRrAxqXvSpDdZhIKsH9Fi
 unzip elrond-archive.zip 1> /dev/null
