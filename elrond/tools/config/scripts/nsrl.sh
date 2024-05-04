21313d1a0b4fb13f2517bd53d4ee8a1c082b2e0a
Ben Smith <ezaspy@proton.me>
2024-05-04 20:54:08 +0100

    Update nsrl.sh

--- elrond/tools/config/scripts/nsrl.sh
+++ elrond/tools/config/scripts/nsrl.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sleep 1
 clear
-printf "\n\n +------------------------------------------- \e[1;31mOPTIONAL\e[m: RDS Hash Sets Download -------------------------------------------+\n | Download the latest \e[1;33mFULL SQL DOWNLOADS\e[m RDS Hash Sets from the following link and save in \e[1;33m/opt/elrond/elrond/tools/\e[m     |\n | \e[0;36mhttps://www.nist.gov/itl/ssd/software-quality-group/national-software-reference-library-nsrl/nsrl-download/current-rds \e[m|\n +------------------------------------------------------------------------------------------------------------------------+\n   Press ENTER to continue...\n"
-read answer
-sleep 1
+printf "\n\n  +--------------------------- \e[1;31mOPTIONAL\e[m: RDS Hash Sets Download ---------------------------+\n\n   \e[0;36m$ wget -O /opt/elrond/elrond/tools/RDS_2024.03.1_modern.zip \\ \n     https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/rds_2024.03.1/RDS_2024.03.1_modern.zip\e[m \n\n\n"
+sleep 20
