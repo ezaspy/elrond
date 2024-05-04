2a3a08d1948887ad3d84fa80428ddbf3c6713814
Ben Smith <ezaspy@proton.me>
2024-05-04 20:54:06 +0100

    Update repo.sh

--- elrond/tools/config/scripts/repo.sh
+++ elrond/tools/config/scripts/repo.sh
@@ -6,13 +6,17 @@ python3 -m pip install gdown
 export PATH="$HOME/.local/bin:$PATH"
 source ~/.bashrc
 clear
-printf "\n  -> Downloading additional elrond repository components...\n\n"
+printf "\n  -> Downloading additional elrond components...\n\n"
 sudo add-apt-repository -y ppa:linuxgndu/sqlitebrowser > /dev/null 2>&1 # db browser for sqlite
 
 # downloading additional tools for elrond
 gdown https://drive.google.com/uc?id=1mVrkLp84jchHRrAxqXvSpDdZhIKsH9Fi
-unzip elrond-archive.zip 1> /dev/null
-rm -rf __MACOSX/
+if [ -f "elrond-archive.zip" ]; then
+    unzip elrond-archive.zip 1> /dev/null
+    rm -rf __MACOSX/
+else
+    printf "\n 'elrond-archive.zip' did not download successfully.\n Exiting. Please run 'sudo /opt/elrond/./make.sh' again."
+fi
 
 #/opt/elrond/elrond/tools/
 unzip tools.zip 1> /dev/null
@@ -29,8 +33,7 @@ sudo mv volatility3 /opt/elrond/elrond/rivendell/memory/
 
 #/opt/elrond/elrond/rivendell/post/splunk/
 unzip apps.zip 1> /dev/null
-
-#sudo mkdir /opt/elrond/elrond/rivendell/post/splunk/apps/
+sudo mkdir /opt/elrond/elrond/rivendell/post/splunk/apps/
 sudo mv /tmp/apps/*.py /opt/elrond/elrond/rivendell/post/splunk/apps/
 sudo rm -rf /tmp/*.zip
 cd ~
