7ecaf117f1d2b7c993889ffaa4d1d3b70b680f14
Ben Smith <ezaspy@proton.me>
2024-05-04 20:57:59 +0100

    Update nsrl.sh

--- elrond/tools/config/scripts/nsrl.sh
+++ elrond/tools/config/scripts/nsrl.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-sleep 1
-clear
-printf "\n\n  +--------------------------- \e[1;31mOPTIONAL\e[m: RDS Hash Sets Download ---------------------------+\n\n   \e[0;36m$ wget -O /opt/elrond/elrond/tools/RDS_2024.03.1_modern.zip \\ \n     https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/rds_2024.03.1/RDS_2024.03.1_modern.zip\e[m \n\n\n"
-sleep 20
+wget -O /opt/elrond/elrond/tools/RDS_2024.03.1_modern.zip https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/rds_2024.03.1/RDS_2024.03.1_modern.zip
