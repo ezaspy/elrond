97de6f502b3b168e3fcf7725b1eadfe418c5f63f
Ben Smith <ezaspy@proton.me>
2024-05-05 09:48:53 +0100

    Create cloud.sh

---
+++ elrond/tools/config/scripts/cloud.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install azure cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+
+# install gcp cli
+sudo snap install google-cloud-cli
