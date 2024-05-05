6f72be77dfe0e144952e011048f981f146db2b7c
Ben Smith <ezaspy@proton.me>
2024-05-05 20:46:14 +0100

    Delete splunk.sh

--- elrond/tools/config/scripts/splunk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep 1
-clear
-printf "\n  -> Downloading Splunk...\n\n"
-sudo wget -O /opt/elrond/elrond/tools/.splunk.deb "https://download.splunk.com/products/splunk/releases/9.0.5/linux/splunk-9.0.5-e9494146ae5c-linux-2.6-amd64.deb"
-sleep 1
