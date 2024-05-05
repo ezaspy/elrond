f695742b495a185725625e8cbb7949e5de4175f7
Ben Smith <ezaspy@proton.me>
2024-05-05 20:46:10 +0100

    Update elastic.sh

--- elrond/tools/config/scripts/elastic.sh
+++ elrond/tools/config/scripts/elastic.sh
@@ -2,16 +2,6 @@
 sleep 1
 clear
 printf "\n  -> Installing & configuring elasticsearch...\n\n"
-sudo apt install curl jq openjdk-16-jre-headless -y
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt install apt-transport-https -y
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
-#echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-#sudo /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token --scope kibana
-#sudo /usr/share/kibana/bin/kibana-verification-code
-#sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic
-sudo apt update
-sudo apt install elasticsearch kibana
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
 sudo /bin/systemctl enable kibana.service
