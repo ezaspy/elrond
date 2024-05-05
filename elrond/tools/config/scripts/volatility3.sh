e997dae8faae403e847431120712064dc2407ed2
Ben Smith <ezaspy@proton.me>
2024-05-05 20:46:22 +0100

    Update volatility3.sh

--- elrond/tools/config/scripts/volatility3.sh
+++ elrond/tools/config/scripts/volatility3.sh
@@ -2,8 +2,8 @@
 sleep 1
 clear
 printf "\n  -> Installing & configuring volatility3 and dependancies...\n\n"
-#sudo apt install build-essential libreadline-dev libncursesw5-dev libssl-dev libc6-dev libffi-dev zlib1g-dev -y --fix-missing
-#sudo apt-get install checkinstall libgdbm-dev libreadline-dev libnss3-dev libsqlite3-dev tk-dev liblzma-dev -y --fix-missing
+# sudo apt install build-essential libreadline-dev libncursesw5-dev libssl-dev libc6-dev libffi-dev zlib1g-dev -y --fix-missing
+# sudo apt-get install checkinstall libgdbm-dev libreadline-dev libnss3-dev libsqlite3-dev tk-dev liblzma-dev -y --fix-missing
 git clone https://github.com/volatilityfoundation/volatility3.git
 sudo mv volatility3/ /usr/local/lib/python3.8/dist-packages
 git clone https://github.com/JPCERTCC/Windows-Symbol-Tables.git
