7df7919ba3af8a522cbb00aeda13718bc668f42c
Ben Smith <ezaspy@proton.me>
2024-05-05 20:46:17 +0100

    Update indx.sh

--- elrond/tools/config/scripts/indx.sh
+++ elrond/tools/config/scripts/indx.sh
@@ -2,9 +2,6 @@
 sleep 1
 clear
 printf "\n  -> Configuring python3.9...\n\n"
-yes '' | sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.9 python3.9-venv -y --fix-missing
 PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 sudo git clone https://github.com/harelsegev/INDXRipper /opt/elrond/elrond/tools/INDXRipper
 /usr/bin/python3.9 -m pip install --upgrade pip
