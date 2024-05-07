e6430ee4e93f25c907d8e4723edfbe73d5e9932d
Ben Smith <ezaspy@proton.me>
2024-05-07 11:28:45 +0100

    Update finish.sh

--- elrond/tools/config/scripts/finish.sh
+++ elrond/tools/config/scripts/finish.sh
@@ -3,8 +3,14 @@
 sleep 1
 sudo apt update
 echo fs.inotify.max_user_watches=1048576 | sudo tee -a /etc/sysctl.conf
+
+# configure .bashrc
+sudo chown +x /opt/elrond/elrond/tools/config/custom.sh
 echo '
-export PS1="\e[1;36m\u@\h:\e[m \e[0;32m\w\e[m\n$ "' >> /home/$USER/.bashrc
+export PS1="\e[1;36m\u@\h:\e[m \e[0;32m\w\e[m\n$ "
+
+/opt/elrond/elrond/tools/config/./elrond.sh' >> /home/$USER/.bashrc
+source ~/.bashrc
 sudo chmod -R 755 /opt/elrond/
 sudo chown -R $USER:$USER /opt/elrond
 sudo chmod +x /opt/elrond/elrond/elrond.py
@@ -15,20 +21,16 @@ sleep 2
 sudo chmod 644 /etc/sysctl.conf
 sudo du -sh /var/cache/apt/archives
 sudo apt update
-sudo apt install bless flameshot cutter-re mlocate -y --fix-missing
+sudo apt install bless flameshot cutter-re mlocate vim -y --fix-missing
 sudo apt-get remove --auto-remove --purge thunderbird rhythmbox yelp libreoffice* kdeconnect aisleriot gnome-mines gnome-sudoku gnome-mahjongg cheese ghex simple-scan wxhexeditor scite -y
 sudo apt-get autoremove --purge
 sudo apt-get clean
 sudo apt update
 sudo updatedb
-sleep 2
+sleep 1
 
-# change desktop background
-gsettings set org.gnome.desktop.background picture-uri file:///opt/elrond/elrond/images/elrond_background.jpg
-gsettings set org.gnome.shell favorite-apps "['org.gnome.seahorse.Application.desktop', 'org.gnome.Nautilus.desktop', 'org.flameshot.Flameshot.desktop', 'firefox_firefox.desktop', 'firefox.desktop', 'org.gnome.Terminal.desktop', 'code.desktop', 'bless.desktop', 'wireshark.desktop', 'cutter-re.desktop', 'sqlitebrowser_sqlitebrowser.desktop', 'maltego.desktop']"
-sleep 2
-
-# configure .bashrc
+# configure terminal to launch on login
+sudo rm -rf gnome-terminal.desktop
 echo "[Desktop Entry]
 Type=Application
 Exec=gnome-terminal
@@ -39,6 +41,8 @@ Name[en_NG]=Terminal
 Name=Terminal
 Comment[en_NG]=Start Terminal On Startup
 Comment=Start Terminal On Startup" > gnome-terminal.desktop
+sudo chmod 755 gnome-terminal.desktop
+sudo chown -R $USER:$USER gnome-terminal.desktop
 sleep 2
 
 sudo rm -rf /home/$USER/.config/autostart/gnome-terminal.desktop
