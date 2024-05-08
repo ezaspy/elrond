0504ce0224fd392e73be6bea971a6f70c5b99c2c
Ben Smith <ezaspy@proton.me>
2024-05-08 12:52:58 +0100

    Update indx.sh

--- elrond/tools/config/scripts/indx.sh
+++ elrond/tools/config/scripts/indx.sh
@@ -8,15 +8,25 @@ python3 -m keyring --disable
 
 # installing python libraries
 USER=$(whoami)
-python3 -m pip install --upgrade pip && sudo python3 -m pip install --upgrade pip
-python3 -m pip install requests pandas openpyxl jupyterlab notebook voila && sudo python3 -m pip install requests pandas openpyxl jupyterlab notebook voila
-sudo chmod -R 744 /opt/elrond/ && sudo chown -R $USER:$USER /opt/elrond
-cd /opt/etl-parser && sudo python3 -m pip install -e . && cd /opt/elrond/elrond
+python3 -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade pip
+python3 -m pip install requests pandas openpyxl jupyterlab notebook voila
+sudo python3 -m pip install requests pandas openpyxl jupyterlab notebook voila
+sudo chmod -R 744 /opt/elrond/
+sudo chown -R $USER:$USER /opt/elrond
+cd /opt/etl-parser
+sudo python3 -m pip install -e .
+cd /opt/elrond/elrond
 
 PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 sudo git clone https://github.com/harelsegev/INDXRipper /opt/elrond/elrond/tools/INDXRipper
-/usr/bin/python3.9 -m pip install --upgrade pip && /usr/bin/python3.8 -m pip install --upgrade pip && /usr/bin/python3 -m pip install --upgrade pip && /usr/bin/python -m pip install --upgrade pip
-python3.9 -m pip install setuptools && python3.9 -m pip install construct==2.10.68
-python3.8 -m pip install setuptools && python3.8 -m pip install construct==2.10.68
+/usr/bin/python3.9 -m pip install --upgrade pip
+/usr/bin/python3.8 -m pip install --upgrade pip
+/usr/bin/python3 -m pip install --upgrade pip
+/usr/bin/python -m pip install --upgrade pip
+python3.9 -m pip install setuptools
+python3.9 -m pip install construct==2.10.68
+python3.8 -m pip install setuptools
+python3.8 -m pip install construct==2.10.68
 python3 -m pip install construct
 sleep 1
