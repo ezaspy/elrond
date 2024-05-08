59dbd88f9cae06d2bae420358c83b2bd6ef79be4
Ben Smith <ezaspy@proton.me>
2024-05-08 08:46:40 +0100

    Update elrond.sh

--- elrond/tools/config/elrond.sh
+++ elrond/tools/config/elrond.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-USER=$(echo $USERNAME)
 # change desktop background
-sudo -u USERNAME bash -c gsettings set org.gnome.desktop.background picture-uri file:///opt/elrond/elrond/images/elrond_background.jpg
+gsettings set org.gnome.desktop.background picture-uri file:///opt/elrond/elrond/images/elrond_background.jpg
 
 # change favourite apps
 gsettings set org.gnome.shell favorite-apps "['org.gnome.seahorse.Application.desktop', 'org.gnome.Nautilus.desktop', 'org.flameshot.Flameshot.desktop', 'firefox_firefox.desktop', 'firefox.desktop', 'org.gnome.Terminal.desktop', 'code.desktop', 'bless.desktop', 'wireshark.desktop', 'cutter-re.desktop', 'sqlitebrowser_sqlitebrowser.desktop', 'maltego.desktop']"
