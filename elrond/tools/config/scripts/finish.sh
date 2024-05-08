690ca36b0e9ff9abc37b4c68a2176a67b45bea65
Ben Smith <ezaspy@proton.me>
2024-05-08 08:40:22 +0100

    Update finish.sh

--- elrond/tools/config/scripts/finish.sh
+++ elrond/tools/config/scripts/finish.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 USER=$(echo $USERNAME)
-sudo apt update
-echo fs.inotify.max_user_watches=1048576 | sudo tee -a /etc/sysctl.conf
 
 # configure .bashrc
-sudo chmod +x /opt/elrond/elrond/tools/config/elrond.sh
-/opt/elrond/elrond/tools/config/./elrond.sh
+sudo -u $USER bash -c '/opt/elrond/elrond/tools/config/./elrond.sh'
 echo '
 export PS1="\e[1;36m\u@\h:\e[m \e[0;32m\w\e[m\n$ "
 
 /opt/elrond/elrond/tools/config/./elrond.sh' >> /home/$USER/.bashrc
-source ~/.bashrc
-sudo chmod -R 755 /opt/elrond/
-sudo chown -R $USER:$USER /opt/elrond
-sudo chmod +x /opt/elrond/elrond/elrond.py
 echo "export PATH=$PATH:/opt/elrond/elrond" >> /home/$USER/.bashrc
+source ~/.bashrc
 
-# removing uneeded applications
-sudo chmod 644 /etc/sysctl.conf
+# cleaning uneeded applications
 sudo du -sh /var/cache/apt/archives
 sudo apt update && sudo apt-get clean && sudo apt update && sudo updatedb
 
