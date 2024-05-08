3428e64cd1ce0866504d95a2a40902a487994a2a
Ben Smith <ezaspy@proton.me>
2024-05-08 16:15:33 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,5 +7,4 @@ sudo chmod -R 744 /opt/elrond/
 sudo chown -R "$USERPROFILE":"$USERPROFILE" /opt/elrond/
 sudo chmod +x /opt/elrond/elrond/config.sh
 sudo chmod +x /opt/elrond/elrond/elrond.py
-sudo cp /opt/elrond/elrond/elrond.sh ~/elrond.sh
 sudo /opt/elrond/elrond/./config.sh
