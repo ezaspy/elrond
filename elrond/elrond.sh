b06e1648f263da197825efe47f47a1df2665248a
Ben Smith <ezaspy@proton.me>
2024-05-08 15:43:48 +0100

    Update elrond.sh

--- elrond/elrond.sh
+++ elrond/elrond.sh
@@ -3,9 +3,6 @@
 USERPROFILE=$(cat /etc/passwd | grep 1000 | cut -d ":" -f 1)
 HOSTNAME=$(hostname)
 
-sudo chmod -R 744 /opt/elrond/
-sudo chown -R "$USERPROFILE":"$USERPROFILE" /opt/elrond
-
 # change desktop background
 gsettings set org.gnome.desktop.background picture-uri file:///opt/elrond/elrond/images/elrond_background.jpg
 
