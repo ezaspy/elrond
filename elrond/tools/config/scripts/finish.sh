6812ec5aaf2d9b115337542c86b7395406f912cd
Ben Smith <ezaspy@proton.me>
2024-05-08 15:09:51 +0100

    Update finish.sh

--- elrond/tools/config/scripts/finish.sh
+++ elrond/tools/config/scripts/finish.sh
@@ -4,12 +4,12 @@ HOST=$(hostname)
 
 # configure .bashrc
 echo '
-export PS1="\e[1;36m\u@\h:\e[m \e[0;32m\w\e[m\n$ "' >> /home/$USER/.bashrc
-echo "export PATH=$PATH:/opt/elrond/elrond" >> /home/$USER/.bashrc
+export PS1="\e[1;36m\u@\h:\e[m \e[0;32m\w\e[m\n$ "' >> /home/$(whoami)/.bashrc
+echo "export PATH=$PATH:/opt/elrond/elrond" >> /home/$(whoami)/.bashrc
 source ~/.bashrc
 
 # configure terminal to launch on login
-sudo rm -rf /home/$USER/.config/autostart/gnome-terminal.desktop
+sudo rm -rf /home/$(whoami)/.config/autostart/gnome-terminal.desktop
 sudo rm -rf gnome-terminal.desktop
 echo "[Desktop Entry]
 Type=Application
@@ -21,19 +21,15 @@ Name[en_NG]=Terminal
 Name=Terminal
 Comment[en_NG]=Start Terminal On Startup
 Comment=Start Terminal On Startup" > gnome-terminal.desktop
-sudo chmod 755 gnome-terminal.desktop
+sudo chmod 744 gnome-terminal.desktop
 sudo chown -R $(whoami):$(whoami) gnome-terminal.desktop
-mkdir -p /home/$USER/.config/autostart
-sudo mv gnome-terminal.desktop /home/$USER/.config/autostart/
-sudo chmod 755 /home/$USER/.config/autostart/gnome-terminal.desktop
+mkdir -p /home/$(whoami)/.config/autostart
+sudo mv gnome-terminal.desktop /home/$(whoami)/.config/autostart/
+sudo chmod 744 /home/$(whoami)/.config/autostart/gnome-terminal.desktop
 
 # cleaning uneeded applications
 sudo du -sh /var/cache/apt/archives
 sudo apt update
 sudo apt-get clean
 sudo apt update
-sudo updatedb
-sudo chmod -R 744 /opt/elrond/
-sudo chown -R $(whoami):$(whoami) /opt/elrond
-sudo chmod +x /opt/elrond/elrond/elrond.py
 sudo updatedb
